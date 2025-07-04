7fa903fc4598ad731bef66b04b39103cf2b3ac6c
Jorge Sosa <joregesosa@gmail.com>
2025-07-04 00:31:03 -0400

    Refactor Header component to manage dark mode state internally

--- resources/js/components/pre-registration/Header.tsx
+++ resources/js/components/pre-registration/Header.tsx
@@ -1,12 +1,23 @@
 import { Moon, Sun } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react";
 
-interface HeaderProps {
-  isDarkMode: boolean;
-  toggleDarkMode: () => void;
-}
 
-export function Header({ isDarkMode, toggleDarkMode }: HeaderProps) {
+export function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode)
+  }
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [isDarkMode])
+
   return (
     <header className="w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between px-4">
@@ -14,9 +25,9 @@ export function Header({ isDarkMode, toggleDarkMode }: HeaderProps) {
           <div className="w-8 h-8 rounded-full funval-gradient flex items-center justify-center">
           </div>
           <div className="w-35 h-10">
-            
+
             <img src="/Sinfondo.png" alt="imagenFunval" />
-            
+
           </div>
         </div>
 
