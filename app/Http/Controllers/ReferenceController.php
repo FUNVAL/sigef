a249fab0cdbd420edb13ae26ee2f3a331b3526f0
Jorge Sosa <joregesosa@gmail.com>
2025-07-26 01:05:56 -0400

    Feature/improve success message clarity in store method of ReferenceController

--- app/Http/Controllers/ReferenceController.php
+++ app/Http/Controllers/ReferenceController.php
@@ -72,7 +72,7 @@ class ReferenceController extends Controller
 
             $message =  [
                 'type' => 'success',
-                'message' => "Gracias por tu referencia, Uno de nuestros representante estara contactando a tu referido entre las proximas 24-72 horas para brindarle toda la información del programa."
+                'message' => "¡Gracias por tu referencia!\n\nValoramos mucho que hayas pensado en alguien para compartir esta oportunidad. Queremos que sepas  que uno de nuestros representantes se comunicará directamente con tu referido en las próximas 72  horas para brindarle toda la información sobre el programa y acompañarlo en este proceso."
             ];
 
             return  back()->with('success', $message);
