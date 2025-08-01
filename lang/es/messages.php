117ea295b8412c045afeff1094648c7cf2cf4bad
Jorge Sosa <joregesosa@gmail.com>
2025-08-01 15:06:21 -0400

    Feature: add Spanish localization files for enums, forms, and messages

---
+++ lang/es/messages.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'success' => [
+        'pre_inscription_success' => '<strong>¡Gracias por su aplicación!</strong><br/><span class="text-blue-600 font-bold">Uno de nuestros representantes se contactará con usted en las próximas 72 horas</span> para brindarle toda la información sobre el programa y resolver cualquier pregunta que pueda tener.<br/>Apreciamos su interés y estamos emocionados de acompañarle en este proceso.',
+
+        'reference_created' => '<strong>¡Gracias por su referencia!</strong><br/>Realmente apreciamos que haya pensado en alguien para compartir esta oportunidad. Queremos que sepa que <span class="text-blue-600 font-bold">uno de nuestros representantes contactará a su persona referida directamente en las próximas 72 horas</span> para brindarle toda la información sobre el programa y guiarle en este proceso.',
+    ],
+
+    'rejections' => [
+        'working' => '<strong>Gracias por su interés en el programa FUNVAL.</strong><br/>Debido a la naturaleza intensiva de nuestras capacitaciones, este programa está dirigido a personas que actualmente se encuentran desempleadas.<br/><br/>Si en el futuro se encuentra buscando empleo, por favor no dude en contactarnos nuevamente.<br/><br/>Adicionalmente, compartimos los siguientes enlaces sobre organizaciones aliadas con FUNVAL que podrían ser de su interés: <a href="https://www.the-academy.org/contact/" target="_blank" class="text-blue-600 underline">The Academy</a> y <a href="https://mentorseducation.org/" target="_blank" class="text-blue-600 underline">Mentors</a>.',
+
+        'entrepreneur' => '<strong>¡Excelente!</strong><br/>Pronto recibirá información de nuestras organizaciones aliadas especializadas en emprendimiento, que comparten con FUNVAL el compromiso de fomentar nuevas oportunidades para personas como usted.<br/><br/>Visite los siguientes enlaces para obtener más información sobre estas organizaciones: <a href="https://www.the-academy.org/contact/" target="_blank" class="text-blue-600 underline">The Academy</a> y <a href="https://mentorseducation.org/" target="_blank" class="text-blue-600 underline">Mentors</a>.',
+
+        'online_part_time' => '<strong>FUNVAL mantiene alianzas con empresas que requieren modalidad de trabajo presencial.</strong><br/>Si en el futuro esta opción se ajusta a su situación, por favor no dude en contactarnos nuevamente. Estaremos encantados de apoyarle en su proceso de búsqueda laboral.<br/><br/>Adicionalmente, compartimos los siguientes enlaces sobre organizaciones aliadas con FUNVAL que podrían ser de su interés: <a href="https://www.the-academy.org/contact/" target="_blank" class="text-blue-600 underline">The Academy</a> y <a href="https://mentorseducation.org/" target="_blank" class="text-blue-600 underline">Mentors</a>.',
+
+        'part_time' => '<strong>Debido a la intensidad de los programas FUNVAL</strong>, los participantes están obligados a tener una conexión continua y estar disponibles sin realizar otras actividades en paralelo durante las horas de capacitación.<br/><br/>Si en el futuro esta opción se ajusta a su situación, por favor no dude en contactarnos nuevamente. Estaremos encantados de apoyarle en su proceso de búsqueda laboral.<br/><br/>Adicionalmente, compartimos la siguiente información sobre organizaciones aliadas con FUNVAL que podrían ser de su interés: <a href="https://www.the-academy.org/contact/" target="_blank" class="text-blue-600 underline">The Academy</a> y <a href="https://mentorseducation.org/" target="_blank" class="text-blue-600 underline">Mentors</a>.',
+    ],
+
+    'duplicates' => [
+        'rejected' => '<strong>Este correo ha sido registrado previamente.</strong><br/>Hemos identificado que ya existe una solicitud asociada con esta dirección de correo, la cual fue evaluada previamente con el siguiente resultado:',
+
+        'pending' => '<strong>Ya existe una solicitud pendiente asociada con esta dirección de correo.</strong><br/>Por favor espere a que uno de nuestros representantes se contacte con usted. El tiempo estimado de contacto es de hasta 72 horas.<br/><br/>Si ese tiempo ya ha transcurrido y aún no ha recibido respuesta, puede escribirnos al siguiente número: [**].<br/><br/>Agradecemos su paciencia y su interés en el programa.',
+    ]
+];
