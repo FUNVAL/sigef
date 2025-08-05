953274b9983810d3a958836726971ef61e2f428f
J-javier <jose360pelico@gmail.com>
2025-08-05 11:48:50 -0600

    style: update background styles in AuthSimpleLayout component for improved aesthetics

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -9,8 +9,8 @@ interface AuthLayoutProps {
 
 export default function AuthSimpleLayout({ children, title, description }: PropsWithChildren<AuthLayoutProps>) {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-blue-50 via-indigo-50 to-blue-100 p-6 text-black">
-            <div className="w-120 rounded-2xl px-8 py-10 bg-white/80 border border-blue-100 shadow-2xl"> 
+        <div className="flex min-h-screen items-center justify-center bg-gray-200 p-6 text-black">
+            <div className="w-120 rounded-2xl px-8 py-10 bg-gray-50 border border-blue-100 shadow-2xl"> 
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
